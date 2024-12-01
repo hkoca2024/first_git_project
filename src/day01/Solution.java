ec8eb3b98575495cda3b22bc5193596105403e18
90542 <hamit.koca@gmail.com>
2024-12-01 22:59:51 +0300

    second

---
+++ src/day01/Solution.java
@@ -0,0 +1,22 @@
+package day01;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        int []  arr={1,3,4,5};
+
+                Arrays.sort(arr);
+                int n=arr[arr.length-1];
+                int sum=n*(n+1)/2;
+                int total=0;
+                for(int i=0;i<arr.length;i++){
+                    total+=arr[i];
+                }
+                if(sum==total){
+                    System.out.println(n+1);
+                }
+        System.out.println(sum-total);
+            }
+        }
+
+
