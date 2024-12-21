99c640e7318144c5d657496c93776e595c818d39
90542 <hamit.koca@gmail.com>
2024-12-22 01:34:27 +0300

    third

--- src/day01/Solution.java
+++ src/day01/Solution.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Solution {
     public static void main(String[] args) {
-        int []  arr={1,3,4,5};
+        int []  arr={1,2,3,5};
 
                 Arrays.sort(arr);
                 int n=arr[arr.length-1];
